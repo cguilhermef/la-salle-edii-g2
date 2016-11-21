0e1e40e989054817622ae09ff510598715add457
Christian Guilherme Fritsch <guilherme@opiniaoearte.com.br>
2016-11-21 13:14:16 -0200

    Está bugado. Compactação pode estar gerando uma saída inválida

--- main.c
+++ main.c
@@ -28,29 +28,128 @@ void compactarArquivo () {
 	* assim, sequências que podem ser otimizadas.
 	*/
 	char now;
-	char after;
-	int size = 0;
+	char before = '@';
 	int count = 0;
-	
-	char buffer[1000];
-	memset(buffer, '\0', 1000);
-	
 	while(!feof(originalFile)) {
 		now = getc(originalFile);
 		if (count == 0) {
-			after = now;
+			/**
+			* se a contagem é zero, significa que o caracter anterior
+			* já foi resolvido e deve ser iniciado um novo ciclo
+			*/
+			before = now;
 			count++;
-		} else if (now == after) {
+		} else if (now == before) {
+			/**
+			* se o caracter atual é igual ao anterior, temos o início
+			* de uma sequência. Apenas é incrementado o contador
+			*/
 			count++;
 		} else if (count == 1) {
-			putc(now, newFile);
+			/** 
+			* Caso o caracter atual seja diferente do atual,
+			* e o caracter anterior ocorreu apenas uma vez,
+			* não houve sequência. O caracter anterior é impresso
+			* e o atual passa a ser o anterior.
+			*/
+			putc('@', newFile);
+			putc(before, newFile);
+			before = now;
 		} else {
-			fprintf(newFile, "@%d%c", count, now);
-			count = 0;
+			/**
+			* Não caindo em nenhum dos IF anteriores, significa que 
+			* há uma sequência de caracter repetido, mas que o caracter
+			* atual difere do anterior. A sequência então é resumida e 
+			* impressa, com o caracter anterior. O caracter atual passa
+			* a ser o anterior.
+			*/
+			fprintf(newFile, "@%d%c", count, before);
+			before = now;
+			count = 1;
 		}
 	}
 	fclose(originalFile);
 	fclose(newFile);
+	return;
+}
+
+void descompactarArquivo() {
+	FILE * compressedFile = fopen("compactado.txt", "r");
+	if (compressedFile == NULL) {
+		printf("\n Erro ao abrir o arquivo de origem!\n");
+		return;
+	}
+	
+	/** 
+	* Arquivo de destino é aberto com w+ para garantir 
+	* que ele será criado vazio.
+	*/
+	FILE * uncompressedFile = fopen("descompactado.txt", "w+");
+	fclose(uncompressedFile);
+	/**
+	* Então é aberto novamente, em modo append 
+	*/
+	uncompressedFile = fopen("descompactado.txt", "a");
+	if (uncompressedFile == NULL) {
+		printf("\n Erro ao criar arquivo de destino!\n");
+		return;
+	}
+	
+	char buffer[100];
+	memset(buffer, '\0', 100);
+	char num[100];
+	memset(num, '\0', 100);
+	
+	char ch;
+	char now;
+	int size = 0;
+	int c = 0;
+	int n = 0;
+	
+	while(!feof(compressedFile)) {
+		now = getc(compressedFile);
+		if (now == '@') {
+			if (size == 0) {
+				memset(buffer, '\0', 100);
+				memset(num, '\0', 100);
+			} else {
+				if (size == 1) {
+					putc(ch, uncompressedFile);
+				} else {
+					strncpy(num, buffer, size-1);
+					n = atoi(num);
+					ch = buffer[size-1];
+					c = 0;
+					while(c < n) {
+						fprintf(uncompressedFile, "%c", ch);
+						c++;
+					}
+				}
+				memset(buffer, '\0', 100);
+				size = 0;
+			}
+		} else {
+			buffer[size] = now;
+			size++;
+		}
+	}
+	
+	if (size == 1) {
+		putc(ch, uncompressedFile);
+	}
+	if (size > 1){
+		strncpy(num, buffer, size-1);
+		n = atoi(num);
+		ch = buffer[size-1];
+		c = 0;
+		while(c < n) {
+			putc(ch, uncompressedFile);
+			c++;
+		}
+	}
+	
+	fclose(compressedFile);
+	fclose(uncompressedFile);
 	return;
 }
 
@@ -75,6 +174,7 @@ int main(int argc, char *argv[]) {
 			}
 			case 2: {
 				printf("Descompactando...\n");
+				descompactarArquivo();
 				break;
 			}
 			case 0: {
