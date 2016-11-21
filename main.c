6f98530092fae3e6f92f2ef6043599242a2ced45
Christian Guilherme Fritsch <guilherme@opiniaoearte.com.br>
2016-11-21 09:02:22 -0200

    Implementado o menu principal; implementado o básico do método compactarArquivo

--- main.c
+++ main.c
@@ -1,22 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void compactarArquivo () {
+	FILE * originalFile = fopen("teste.txt", "r");
+	if (originalFile == NULL) {
+		printf("\n Erro ao abrir o arquivo de origem!\n");
+		return;
+	}
+	/** 
+	* Arquivo de destino é aberto com w+ para garantir 
+	* que ele será criado vazio.
+	*/
+	FILE * newFile = fopen("compactado.txt", "w+");
+	fclose(newFile);
+	/**
+	* Então é aberto novamente, em modo append 
+	*/
+	newFile = fopen("compactado.txt", "a");
+	if (newFile == NULL) {
+		printf("\n Erro ao criar arquivo de destino!\n");
+		return;
+	}
+	/**
+	* Percorre todo o arquivo original, caracter a caracter,
+	* comparando o caracter anterior com o atual, identificando
+	* assim, sequências que podem ser otimizadas.
+	*/
+	char now;
+	char after;
+	int size = 0;
+	int count = 0;
+	
+	char buffer[1000];
+	memset(buffer, '\0', 1000);
+	
+	while(!feof(originalFile)) {
+		now = getc(originalFile);
+		if (count == 0) {
+			after = now;
+			count++;
+		} else if (now == after) {
+			count++;
+		} else if (count == 1) {
+			putc(now, newFile);
+		} else {
+			fprintf(newFile, "@%d%c", count, now);
+			count = 0;
+		}
+	}
+	fclose(originalFile);
+	fclose(newFile);
+	return;
+}
 
 int main(int argc, char *argv[]) {
 	
-	FILE * f = fopen("lorem.txt", "r");
-	FILE * cpyF = f;
-	printf("Carregando...");
-	if (f) {
-		printf("feito!\n");
-	}
+	int choice = -1;
 	
-	printf("\n %d \n\n", sizeof(f));
-	while(!feof(cpyF)) {
-		printf("%c", getc(cpyF));
+	while(choice != 0) {
+		printf("Escolha uma opcao:\n\n");
+		printf("1 - Compactar \n");
+		printf("2 - Descompactar \n");
+		printf("0 - Sair\n\n> ");
+		scanf("%d", &choice);
+		fflush(stdin);
+		
+		switch(choice) {
+			case 1: {
+				printf("Compactando...");
+				compactarArquivo();
+				printf(" feito!\n");
+				break;
+			}
+			case 2: {
+				printf("Descompactando...\n");
+				break;
+			}
+			case 0: {
+				printf("Saindo...\n");
+				break;
+			}
+			default: {
+				printf("Opcao invalida! \n");
+				choice = -1;
+				break;
+			}
+		}
 	}
-	
 	
 	return 0;
 }
